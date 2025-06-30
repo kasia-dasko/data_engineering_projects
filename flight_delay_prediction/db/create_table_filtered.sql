28e0d2106426a076b3e944f974a7db3ede0b410f
kasiadasko <kd140529@student.sgh.waw.pl>
2025-07-01 00:58:53 +0200

    table with new column

---
+++ flight_delay_prediction/db/create_table_filtered.sql
@@ -0,0 +1,7 @@
+CREATE TABLE flights (
+    flight_date DATE,
+    airline_name VARCHAR(10),
+    dep_iata VARCHAR(10),
+    min_delay_dep INTEGER,
+    is_delayed INTEGER
+);
---
+++ flight_delay_prediction/db/create_table_raw_data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE flights_filtered (
+    flight_date DATE,
+    airline_name VARCHAR(10),
+    dep_iata VARCHAR(10),
+    min_delay_dep INTEGER,
+);
