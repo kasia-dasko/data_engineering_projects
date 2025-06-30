d2d935b7937424ffa9e7179ddbb36ee9beaefeec
kasiadasko <kd140529@student.sgh.waw.pl>
2025-07-01 01:01:59 +0200

    code updates

--- flight_delay_prediction/scripts/save_to_warehouse.py
+++ flight_delay_prediction/scripts/save_to_warehouse.py
@@ -7,8 +7,16 @@ def save_to_warehouse(ti):
         raise ValueError("No data")
     
      df = pd.DataFrame(flights)
+     expected_columns = ['flight_date', 'airline_name', 'dep_iata', 'min_delay_dep']
+     df = df[expected_columns]
+
+     df['flight_date'] = pd.to_datetime(df['flight_date'], errors='coerce')
+     df['min_delay_dep'] = pd.to_numeric(df['min_delay_dep'], errors='coerce').fillna(0).astype(int)
+     df['airline_name'] = df['airline_name'].astype(str)
+     df['dep_iata'] = df['dep_iata'].astype(str)
+     
      hook = PostgresHook(postgres_conn_id='postgres_id')
      engine = hook.get_sqlalchemy_engine()
 
-     df.to_sql('flights', engine, if_exists='replace', index=False)
+     df.to_sql('flights', engine, if_exists='append', index=False)
 
